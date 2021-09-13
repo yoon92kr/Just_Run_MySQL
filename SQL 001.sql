8e394b81e802fbd05df786e0ec1f3d8a7ee22fcc
yoon92kr <yoon92_kr@naver.com>
2021-09-13 19:44:07 +0900

    2021.09.13 Creat ▶ use 스키마 명;  == 해당 스키마를 이용 ▶ CREAT TABLE 테이블명 (필드명 자료형(), 필드명 자료형());

---
+++ SQL 001.sql
@@ -0,0 +1,12 @@
+use shopdb;
+ /* create table indextbl (
+first_name varchar(14),
+last_name varchar(16),
+hire_date date);
+
+insert into indextbl
+select first_name, last_name, hire_date
+from employees.employees
+limit 500; */
+
+select * from indextbl;
