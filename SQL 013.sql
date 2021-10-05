63c4fe4a26f9fde4ade1c35cd1df2cce1348790f
set run <73813647+yoon92kr@users.noreply.github.com>
2021-10-06 08:48:49 +0900

    Delete SQL 013.sql

--- SQL 013.sql
+++
@@ -1,8 +0,0 @@
-use test_tbl;	#사용할 데이터베이스 명시
-alter table buy		# 변경할 테이블 명시
-
--- add constraint foreign key (test_id) references member(test_id);
-
-
-modify column test_id int(11) not null;		# modify column [변경할 컬럼명] [변경할 컬럼 타입]
-# 컬럼(필드) 타입 변경하기 int(11) -> int(11) not null
