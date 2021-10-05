02c8731a51afa3685389940efd848f80678effe1
yoon92kr <yoon92_kr@naver.com>
2021-10-05 10:28:18 +0900

    2021.09.13 Trigger ▶ 예시를 위한 선행작업

---
+++ Code007.sql
@@ -0,0 +1,5 @@
+use shopdb;
+# select * from membertbl;
+# insert into membertbl values ('Soccer','흥민','서울시 서대문구 북가좌동');	# 튜플 추가
+# update membertbl set memberaddress='서울 강남구 역삼동'where memberid='soccer';	# 튜플 수정
+# delete from membertbl where memberid = 'soccer';	# 튜플 삭제
