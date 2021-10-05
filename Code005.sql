c3a9f4e85b6cb3ec54ced9fdfbb7dbb7a94ffb9d
yoon92kr <yoon92_kr@naver.com>
2021-10-05 10:27:20 +0900

    2021.09.13 Index ▶ 해당 필드를 중심으로 한 index 생성, tree구조로 생성된다

---
+++ Code005.sql
@@ -0,0 +1,4 @@
+use shopdb;
+# create index idx_indextbl_firstname on indextbl(first_name);	# 인덱스 생성	
+# show index from indextbl;	# 인덱스 보기
+#alter table indextbl drop index indextbl_firstname;	# 인덱스 삭제
