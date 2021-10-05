67d7ff2142fbd2dcd5c5555d339ce1046b36abd9
set run <73813647+yoon92kr@users.noreply.github.com>
2021-10-06 08:49:23 +0900

    Delete SQL 018.sql

--- SQL 018.sql
+++
@@ -1,7 +0,0 @@
--- like (equals_같다)
-
-select emp_no, first_name from employees.employees where emp_no like '1002%';
-# employees 테이블의 emp_no 튜플 중 1002로 시작하는 모든 튜플들의 emp_no, first_name 튜플을 반환
-
-select emp_no, first_name from employees.employees where emp_no like '_____5';
-# employees 테이블의 emp_no 튜플 중 여섯번째 자료가 5인 모든 튜플들의 emp_no, first_name 튜플을 반환
